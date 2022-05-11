d25508bce32022cf46c75c790b16311106bf61bd
jabekl <92928270+jabekl@users.noreply.github.com>
2022-05-11 17:42:44 +0200

    random chicken place

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-var chicken = document.getElementById("chicken-image")
+let mouseCursor = document.querySelector('.cursor');
+window.addEventListener('mousemove',cursor)
 
 function positionChicken() {
-    let random1 = Math.floor(Math.random() * 1000).toString()
-    let random2 = Math.floor(Math.random() * 1900).toString()
-    console.log(random1, random2)
+    let random1 = Math.floor(Math.random() * 900).toString()
+    let random2 = Math.floor(Math.random() * 1800).toString()
+    console.log(chicken)
     chicken.style.top = random1 + "px"
     chicken.style.left = random2 + "px"
 }
 
-positionChicken()
+function cursor(e){
+	mouseCursor.style.top = e.pageY + 'px';
+	mouseCursor.style.left = e.pageX + 'px';
+
+}
+
+window.onload = function() {
+    var chicken = document.querySelectorAll("chicken")
+    positionChicken()
+} 
