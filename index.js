f352bfe759eb0e536677b69e00eeaf23638696b1
jabekl <92928270+jabekl@users.noreply.github.com>
2022-05-11 18:05:43 +0200

    final working version

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-let mouseCursor = document.querySelector('.cursor');
-window.addEventListener('mousemove',cursor)
+var mouseCursor = document.querySelector('.cursor');
 
 function positionChicken() {
     let random1 = Math.floor(Math.random() * 900).toString()
     let random2 = Math.floor(Math.random() * 1800).toString()
-    console.log(chicken)
     chicken.style.top = random1 + "px"
     chicken.style.left = random2 + "px"
 }
@@ -15,7 +13,47 @@ function cursor(e){
 
 }
 
-window.onload = function() {
-    var chicken = document.querySelectorAll("chicken")
+
+
+function explode(x, y) {
+    var particles = 15,
+      explosion = $('<div class="explosion"></div>');
+  
+    $('body').append(explosion);
+  
+    explosion.css('left', x - explosion.width() / 2);
+    explosion.css('top', y - explosion.height() / 2);
+  
+    for (var i = 0; i < particles; i++) {
+      var x = (explosion.width() / 2) + rand(80, 150) * Math.cos(2 * Math.PI * i / rand(particles - 10, particles + 10)),
+        y = (explosion.height() / 2) + rand(80, 150) * Math.sin(2 * Math.PI * i / rand(particles - 10, particles + 10)),
+        color = rand(0, 255) + ', ' + rand(0, 255) + ', ' + rand(0, 255), 
+        elm = $('<div class="particle" style="' +
+          'background-color: rgb(' + color + ') ;' +
+          'top: ' + y + 'px; ' +
+          'left: ' + x + 'px"></div>');
+  
+      if (i == 0) {
+        elm.one('webkitAnimationEnd oanimationend msAnimationEnd animationend', function(e) {
+          explosion.remove();
+        });
+      }
+      explosion.append(elm);
+    }
     positionChicken()
+  }
+  
+  function rand(min, max) {
+    return Math.floor(Math.random() * (max + 1)) + min;
+  }
+
+
+
+window.onload = function() {
+    var chicken = document.getElementById("chicken")
+    positionChicken()
+    window.addEventListener('mousemove',cursor)
+    chicken.addEventListener("click", (e) => {
+        explode(e.pageX, e.pageY);
+    })
 } 
